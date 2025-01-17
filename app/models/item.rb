f9793694dc252f936d1d5af8c74b8d945b51b56a
tgw-3a <tagawayuu@gmail.com>
2025-01-18 00:02:03 +0900

    ActiveHashの導入とモデル設定、viewでの表示まで実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :shipping_date_estimate
+  belongs_to :shipping_fee_burden
   has_one_attached :image
 end
---
+++ app/models/shipping_date_estimate.rb
@@ -0,0 +1,12 @@
+class Shipping_date_estimate < ActiveHash::Base
+  self.data = [
+    {id: 0, name: '---'},
+    {id: 1, name: '1~2日 発送'},
+    {id: 2, name: '2~3日で発送'},
+    {id: 3, name: '4~7日で発送'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+end
+
