af0360778e77103d11d6a0cba2c39cabd0800aa0
tgw-3a <tagawayuu@gmail.com>
2025-01-18 15:26:08 +0900

    itemモデルに必要なバリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,20 @@ class Item < ApplicationRecord
   belongs_to :shipping_date_estimate
   belongs_to :shipping_fee_burden
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40, message: 'is invalid' }
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input numericality characters.' },
+                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid' }
+    validates :description, length: { maximum: 1000, message: 'is invalid' }
+    validates :user_id
+    with_options numericality: { other_than: 0, message: "can't be blank" } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_fee_burden_id
+      validates :prefecture_id
+      validates :shipping_date_estimate_id
+    end
+  end
 end
