69c592d0e7e60e501352a926e2729a2ff87263c4
tgw-3a <tagawayuu@gmail.com>
2025-01-18 14:17:33 +0900

    出品情報の保存機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:image,:name,:price,:description,:category_id,:condition_id,:shipping_fee_burden_id,:prefecture_id,:shipping_date_estimate_id).merge(user_id: current_user.id)
+  end
 end
