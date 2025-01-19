ac04d94313aacf57e78b6d7e5e8223b002f56870
tgw-3a <tagawayuu@gmail.com>
2025-01-19 15:59:05 +0900

    priceのエラーが重複しないよう修正
    integer型のpriceにasdfなどの文字列を入力した際に、railsがasdfを0に変換するため、format: { with: /\A[0-9]+\z/ }では対処できない部分も併せて修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :image
     validates :name, length: { maximum: 40, message: 'is invalid' }
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input numericality characters.' },
-                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid' }
     validates :description, length: { maximum: 1000, message: 'is invalid' }
     validates :user_id
     with_options numericality: { other_than: 0, message: "can't be blank" } do
@@ -22,4 +20,6 @@ class Item < ApplicationRecord
       validates :shipping_date_estimate_id
     end
   end
+  validates :price, presence: true
+  validates :price, numericality: { allow_blank: true, only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 end
