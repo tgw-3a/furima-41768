750c9c1e41d0d34127fcb5d772ac6943c1275db0
tgw-3a <tagawayuu@gmail.com>
2025-01-20 21:37:22 +0900

    商品情報編集機能の実装とビューの調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @items = Item.order(id: 'DESC')
@@ -7,6 +8,19 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def new
@@ -27,5 +41,14 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :price, :description, :category_id, :condition_id, :shipping_fee_burden_id,
                                  :prefecture_id, :shipping_date_estimate_id).merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    if user_signed_in?
+      @item = Item.find(params[:id])
+      redirect_to root_path unless @item.user_id == current_user.id
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
