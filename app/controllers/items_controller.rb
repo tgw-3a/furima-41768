0b9ccbdc898e8e40fbf30796ee4ea32c6bc56b3b
tgw-3a <tagawayuu@gmail.com>
2025-01-21 16:09:17 +0900

    correct_userにdestroyを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order(id: 'DESC')
