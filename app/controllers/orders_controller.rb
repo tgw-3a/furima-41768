4508fda4c364632600a5d76fe5baf44c8407e617
tgw-3a <tagawayuu@gmail.com>
2025-01-21 23:44:20 +0900

    カード情報のトークン化とデータ受け取りの実装
    購入以外でイベント発火しないよう設定
    payjpの初期は一度のみになるよう設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -21,7 +21,7 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(
-      user_id: current_user.id, item_id: params[:item_id]
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
   end
 
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id, :token
 
   with_options presence: true do
     validates :user_id
