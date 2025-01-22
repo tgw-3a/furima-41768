78bdc6554228edcc253567f00d324e2c334faf72
tgw-3a <tagawayuu@gmail.com>
2025-01-22 17:47:17 +0900

    order_addressから不要なコメントを削除

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,12 +1,5 @@
 FactoryBot.define do
   factory :order_address do
-    # postal_code { "123-4567" }
-    # prefecture_id { 1 }
-    # city { "東京都" }
-    # house_number { "1-1-1" }
-    # building_name { "東京ビル" }
-    # phone_number { "0123456789" }
-    # token { "tok_abcdefghijklmnopqrstuvwxyz123456" }
     postal_code { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
     prefecture_id { Faker::Number.between(from: 1, to: 47) }
     city { Gimei.city.to_s }
