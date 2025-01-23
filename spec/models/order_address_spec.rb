e581e8ba17263f0be42fb3d981148da1b5145fe0
tgw-3a <tagawayuu@gmail.com>
2025-01-23 15:49:29 +0900

    MySQLエラー解消設定とsleep 0.1の削除

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe OrderAddress, type: :model do
   before do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:item)
-    sleep 0.1
     @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
   end
 
