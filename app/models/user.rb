4a1410da8f7fe7b60aa98d45613621cb91a7e3f4
tgw-3a <tagawayuu@gmail.com>
2025-01-23 16:37:28 +0900

    新規登録時のエラー重複表示解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,17 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :password, format: { allow_blank: true, with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :last_name,
+              format: { allow_blank: true, with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :first_name,
+              format: { allow_blank: true, with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :last_name_kana,
+              format: { allow_blank: true, with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :first_name_kana,
+              format: { allow_blank: true, with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width characters.' }
     validates :birth_date
   end
 end
